1f5b654881e2602223989da68b6adcfab6750d2e
Tallal sajid <211360@students.au.edu.pk>
2026-02-16 12:54:43 +0500

    added new cors origin

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = express();
 // Enhanced CORS configuration
 const allowedOrigins = [
   'https://hostelhub-frontend-blue.vercel.app',
+  'hostelhub-frontend-tau.vercel.app',
   'https://hostelhub-frontend.vercel.app',
   'http://localhost:3000',
   process.env.FRONTEND_URL,
